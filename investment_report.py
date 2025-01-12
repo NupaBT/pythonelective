f191682b427652ab9c367cea7d01c250c7adc454
Nupa Babu <bnupa03@gmail.com>
2025-01-12 10:06:42 +0530

    investment_report.py

---
+++ investment_report.py
@@ -0,0 +1,6 @@
+revenue=float(input("Enter the revenue: "))
+expense=float(input("Enter the expenses: "))
+profit=revenue-expense
+cost=float(input("Enter the cost: "))
+roi=(profit/cost)*100
+print("The return on investment is",roi,"%")
