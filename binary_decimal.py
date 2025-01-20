f9acfdd1dad6fddca2bf213588fe95794372035f
Nupa Babu <90416462+NupaBT@users.noreply.github.com>
2025-01-20 21:25:20 +0530

    binary_decimal

---
+++ binary_decimal.py
@@ -0,0 +1,8 @@
+binary = input("Enter binary number: ")
+decimal = 0
+power = 0
+
+for digit in binary[::-1]:  
+    decimal += int(digit) * (2 ** power)
+    power += 1
+print("The decimal equivalent of", binary,"is",decimal)
