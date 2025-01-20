439c1252f353fd57e0381c85b0f38096f408e734
Nupa Babu <90416462+NupaBT@users.noreply.github.com>
2025-01-20 21:17:19 +0530

    investment_report

--- investment_report.py
+++ investment_report.py
@@ -2,5 +2,5 @@ revenue=float(input("Enter the revenue: "))
 expense=float(input("Enter the expenses: "))
 profit=revenue-expense
 cost=float(input("Enter the cost: "))
-roi=(profit/cost)*100
-print("The return on investment is",roi,"%")
+Roi=(profit/cost)*100
+print("The return on investment is",Roi,"%")
