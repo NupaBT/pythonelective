af669472684192c7ce0fde8563fd097ce2081387
Nupa Babu <90416462+NupaBT@users.noreply.github.com>
2025-01-20 21:32:45 +0530

    square_root

--- square_root.py
+++ square_root.py
@@ -1,2 +1,13 @@
 a=int(input("enter a number "))
-print("the square root of",a,"is",a**0.5)
+z=1
+if a < 0:
+     print("no square root for negative numbers.")
+else:
+    for num in range(a):
+        if a==(z*z):
+            break
+        z=z+1
+    z= (z+a/z)/2
+    err=abs(a-z**2)
+    if err==0:
+        print("The Square root of ",a ,"is",z)
